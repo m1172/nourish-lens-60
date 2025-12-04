22dac7e890afbd1dc9411f63b29557d1b692d750
m1172 <muhshuh2000@gmail.com>
2025-12-04 21:54:54 +0100

    Everything is ready to use

--- src/components/navigation/index.ts
+++
@@ -1,2 +0,0 @@
-export { default as BottomNav } from './BottomNav';
-export { NavLink } from './NavLink';
--- src/components/onboarding/index.ts
+++
@@ -1,2 +0,0 @@
-export { default as OnboardingModal } from './OnboardingModal';
-export { OnboardingRoute } from './OnboardingRoute';
